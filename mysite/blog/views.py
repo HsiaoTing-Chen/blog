2b51e0fc86ead18bb5bcd5f582e325614c9929b1
Ann.chen <ann.chen@quadas.com>
2017-10-18 16:33:28 +0800

    delete protect

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
-
 import logging
 import logging.config
 import sys
@@ -52,13 +51,19 @@ def article_add(request):
 
 @login_required
 def article_edit(request, article_id):
+
     article_list = Article.objects.order_by('-created_at')[:5]
-    logging.info("article_id= "+article_id)
     article = Article.objects.get(pk=article_id)
     username = request.user.username
-    context = { 'article': article, 'username': username, 'article_list': article_list}
+    if article.author == username:
+        context = {'article': article, 'username': username, 'article_list': article_list}
+        return render(request, 'blog/edit.html', context)
+    else:
+        error_message = "Edit Fail： sorry, you are not author"
+        context = {'error_message': error_message, 'article': article, 'article_list': article_list}
+        return render(request, 'blog/detail.html', context)
 
-    return render(request, 'blog/edit.html', context)
+
 
 @login_required
 def article_edit_save(request, article_id):
@@ -70,16 +75,22 @@ def article_edit_save(request, article_id):
     article.title = title
     article.content = content
     article.save()
-    logging.info(author+" , "+title+" , "+content)
     return HttpResponseRedirect(reverse('blog:index'))
 
 
 @login_required
 def article_delete(request, article_id):
     article = Article.objects.get(pk=article_id)
-    article.is_delete = True
-    article.save()
-    return HttpResponseRedirect(reverse('blog:index'))
+    article_list = Article.objects.order_by('-created_at')[:5]
+    username = request.user.username
+    if article.author == username:
+        article.is_delete = True
+        article.save()
+        return HttpResponseRedirect(reverse('blog:index'))
+    else:
+        error_message = "Delete Fail： sorry, you are not author"
+        context = {'error_message': error_message, 'article': article, 'article_list': article_list}
+        return render(request, 'blog/detail.html', context)
 
 
 @login_required
@@ -89,8 +100,6 @@ def article_save(request):
     content = request.POST['content']
     new_article = Article(author=author,title=title,content=content)
     new_article.save(force_insert=True)
-
-    logging.info(author+" , "+title+" , "+content)
     return HttpResponseRedirect(reverse('blog:index'))
 
 
@@ -123,6 +132,7 @@ def message_save(request, article_id):
     title = request.POST['title']
     content = request.POST['content']
     article = Article.objects.get(id=article_id)
-    article.message_set.create(author=author, title=title,content=content)
+    article.message_set.create(author=author, title=title, content=content)
     return HttpResponseRedirect(reverse('blog:index'))
+
 
